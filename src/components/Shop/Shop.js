37f39cacea029d80e2b222e60def2d2a2f8cf0cc
CoderMahabub <codermahabub@gmail.com>
2021-09-23 07:24:11 +0600

    Shop Component Layout, load data and set state

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Shop.css';
 
 const Shop = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch('./products.JSON')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, []);
     return (
-        <div>
-            <h1>Shop</h1>
+        <div className="shop-container">
+            <div className="product-container">
+                <h3>Products: {products.length}</h3>
+                {
+                    products.map(product => console.log(product))
+                }
+            </div>
+            <div className="cart-container">
+                <h3>Order Summary</h3>
+                <h5>Items Ordered: </h5>
+            </div>
         </div>
     );
 };
